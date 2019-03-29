4427ddabc895031c6bc0e338c0d2835b011b24d7
RQuispeC <quispe@liv.ic.unicamp.br>
2019-03-29 20:17:14 -0300

    Added deconv2d to network initialization

--- architecture/network.py
+++ architecture/network.py
@@ -73,10 +73,12 @@ def weights_normal_init(model, dev=0.01):
             weights_normal_init(m, dev)
     else:
         for m in model.modules():
-            if isinstance(m, nn.Conv2d):                
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
                 #print torch.sum(m.weight)
                 m.weight.data.normal_(0.0, dev)
                 if m.bias is not None:
                     m.bias.data.fill_(0.0)
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0.0, dev)
+
+
